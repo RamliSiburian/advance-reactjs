863b113e231f59b738178c01eee675f2b3fe5e01
RamliSiburian <ramlisiburian1111@gmail.com>
2022-10-13 15:38:43 +0700

    Add navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav } from "react-bootstrap";
 
 // import necessary object from react-router-dom
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
 // import "pages" component here
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     // define <BrowserRouter> as parent element
-    <>
-      <div>
-        {/* Setup navigation here */}
-      </div>
-      {/* code <Routes> and <Route> below, according to path and component */}
-    </>
+    <Router>
+      <Container>
+        <div>
+          {/* Setup navigation here */}
+          <Nav>
+            <ul className="d-flex">
+              <li className="me-5"><Link to="/"> Home </Link> </li>
+              <li className="me-5"><Link to="/About" > About </Link> </li>
+              <li className="me-5"><Link to="/Profile"> Profile </Link> </li>
+            </ul>
+          </Nav>
+        </div>
+        {/* code <Routes> and <Route> below, according to path and component */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/About" element={<About />} />
+          <Route exact path="/Profile" element={<Profile />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
